d7295e0aca817a92c9533748f380bef481ab1218
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-26 17:02:56 -0700

    Step 10

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -25,12 +25,15 @@ if (Meteor.isClient) {
         });
 
         $scope.addTask = function (newTask) {
-          $scope.tasks.push({
-            text: newTask,
-            createdAt: new Date(),
-            owner: Meteor.userId(),
-            username: Meteor.user().username
-          });
+          $meteor.call('addTask', newTask);
+        };
+   
+        $scope.deleteTask = function (task) {
+          $meteor.call('deleteTask', task._id);
+        };
+   
+        $scope.setChecked = function (task) {
+          $meteor.call('setChecked', task._id, !task.checked);
         };
 
         $scope.$watch('hideCompleted', function () {
@@ -46,6 +49,28 @@ if (Meteor.isClient) {
       }
     ]);
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
